b4bba6d2d79b79e8bbf91c635350504b171a257a
Jason Bourget <jason@Jasons-MacBook-Pro.local>
2018-09-12 12:20:48 -0600

    Bare minimum passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+  var result = [];
+  recursivePush(document.body);
+
+  function recursivePush(element) {
+    var classList = element.classList;
+    if (classList) {
+      classList.forEach(function(elementClass) {
+        if (elementClass === className) {
+          result.push(element);
+        }
+      })
+    }
+    if (element.childNodes !== undefined && element.childNodes.length > 0) {
+      var children = element.childNodes
+      children.forEach(function(element) {
+        recursivePush(element);
+      })
+    }
+  }
+  return result;
+}
+
+
+  // You should use document.body, element.childNodes, and element.classList
+  // input: a web page with elements of varying classes
+  // output: an array, like this:
+	// [div.example, div.example.color, div.example.color]
+  // assumptions: we're passing in ONE class, not multiple classes
+  // the elements may have multiple classes
+  // we have to check each class on each element against our target class
+  // if it's a match, we push it to an array
+  // use recursion to dive deeper: if the element in question has children, call the function with the children
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  
+  if (Array.isArray(obj)) {
+    return '[' + stringifyObject(obj) + ']';
+  }
+
+  if (obj instanceof Object) {
+	return '{' + stringifyObject(obj) + '}';
+  };
+
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+
+  return '' + obj;
+}
+
+
+function stringifyObject (obj) {
+  var string = '';
+  _.each(obj, function(value, key) {
+	  	if (string !== '') {
+	  	  string += ','
+	  	}
+	    if (typeof value === 'function' || value === undefined) {
+	  	  return string += '';
+	  	}
+      if (!Array.isArray(obj)) {
+        string += '"' + key + '":'
+      }
+	  	if (typeof value === 'string') {
+	  	  return string += '"' + value + '"';
+	  	}
+	  	if (Array.isArray(value)) {
+	  	  return string += '[' + stringifyObject(value) + ']';
+	  	}
+      if (value instanceof Object) {
+        return string += '{' + stringifyObject(value) + '}';
+      }
+	  	return string += value;  		
+	  })
+  return string
+}
